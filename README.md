62b224e94e8dbed5053eda921759b1aa609cc5d6
Bogdan Bodnaruk <bbodnar@softserveinc.com>
2018-04-05 12:14:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Node Stager tool #
+
+## TODO: ##
+  * Add documentation
+  * Test startup method
+
+### How do I get set up? ###
+
+* Clone project 
+* Run ```yarn install``` for dependencies
+* ```sudo yarn link``` will create a symbolic link between project directory and executable ```Stager``` command
+* Run `stager --version` to check if it works
---
+++ bin/stager.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+const cli = require('commander');
+const Commander = require('../lib/Commander');
+
+Commander.ensure('git');
+Commander.ensure('pm2');
+
+cli
+  .version('1.0.0')
+  .description('node-stager')
+;
+
+cli
+  .command('config [repository]')
+  .option('-a, --all', 'Show all configs')
+  .alias('c')
+  .description('Show configs')
+  .action(Commander.config)
+;
+
+cli
+  .command('deploy <repository> <branch>')
+  .alias('d')
+  .description('Deploy branch')
+  .action(Commander.deploy)
+;
+
+cli
+  .command('remove [repository] [branch]')
+  .option('-a, --all', 'Remove all')
+  .alias('del')
+  .description('Delete all instances')
+  .action(Commander.remove)
+;
+
+cli
+  .command('status')
+  .alias('s')
+  .description('Show all instances')
+  .action(Commander.status)
+;
+
+cli
+  .command('prune <repository>')
+  .alias('p')
+  .description('Prune unused instances')
+  .action(Commander.prune)
+;
+
+cli
+  .command('update <repository> <branch>')
+  .alias('u')
+  .description('Update deployed branch')
+  .action(Commander.update)
+;
+
+cli
+  .command('branches <repository>')
+  .alias('b')
+  .description('Show branches list')
+  .action(Commander.branches)
+;
+
+cli
+  .command('startup')
+  .alias('su')
+  .description('Configure automate startup for pm2 instances')
+  .action(Commander.startup)
+;
+
+cli.parse(process.argv);
