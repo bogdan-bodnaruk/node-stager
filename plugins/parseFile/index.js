f2fc5cebc94e5b93542c881408a96db16e2dd619
Bogdan Bodnaruk <bbodnar@softserveinc.com>
2018-04-17 23:02:48 +0300

    Fix regex for parsing files

--- plugins/parseFile/index.js
+++ plugins/parseFile/index.js
@@ -22,7 +22,10 @@ class ParseIni {
       const inputFile = path.join(this.config.appDir, this.config.input);
       const outputFile = path.join(this.config.appDir, this.config.output);
       const input = fs.readFileSync(inputFile, "utf8");
-      const content = format(input, this.config, { skipUndefined: true });
+      const content = format(input, this.config, {
+        skipUndefined: true,
+        regex: /{(\w*?)}/g,
+      });
 
       fs.writeFileSync(outputFile, content, (err) => {
         if (err) {
